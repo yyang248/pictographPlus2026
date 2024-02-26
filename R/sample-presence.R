aaf030f46c3e6f65aad10294597439bc0edbfe77
jiaying2508 <38696658+jiaying2508@users.noreply.github.com>
2024-02-26 10:26:49 -0500

    Update sample-presence.R

--- R/sample-presence.R
+++ R/sample-presence.R
@@ -1,23 +1,23 @@
-
-#' separate mutations by sample presence using mcf
-#' @export
-separateMutationsByMCF <- function(mcf) {
-  # returns list of lists --
-  # each item of list contains mcf for a mutation sample presence set
-  # original mutation indices from input_data are recorded in $mutation_indices
-  pres <- ifelse(mcf > 0, 1, 0)
-  pat <- apply(pres, 1, function(x) paste0(x, collapse=""))
-  types <- sort(names(table(pat)), decreasing=TRUE)
-  if (length(types) == 1) {
-    type_indices <- list()
-    type_indices[[types]] <- seq_len(length(nrow(mcf_est)))
-  } else {
-    type_indices <- lapply(types, function(x) which(pat == x))
-    names(type_indices) <- types
-  }
-  warning('consider merge one-sample cluster with other clusters')
-  return(type_indices)
-}
+#' 
+#' #' separate mutations by sample presence using mcf
+#' #' @export
+#' separateMutationsByMCF <- function(mcf) {
+#'   # returns list of lists --
+#'   # each item of list contains mcf for a mutation sample presence set
+#'   # original mutation indices from input_data are recorded in $mutation_indices
+#'   pres <- ifelse(mcf > 0, 1, 0)
+#'   pat <- apply(pres, 1, function(x) paste0(x, collapse=""))
+#'   types <- sort(names(table(pat)), decreasing=TRUE)
+#'   if (length(types) == 1) {
+#'     type_indices <- list()
+#'     type_indices[[types]] <- seq_len(length(nrow(mcf_est)))
+#'   } else {
+#'     type_indices <- lapply(types, function(x) which(pat == x))
+#'     names(type_indices) <- types
+#'   }
+#'   warning('consider merge one-sample cluster with other clusters')
+#'   return(type_indices)
+#' }
 
 #' sample presence for MCMC
 #' @export
@@ -25,29 +25,33 @@ separateMutationsBySamplePresence <- function(input_data, min_mutation_per_box=2
   # returns list of lists -- 
   # each item of list contains input data for a mutation sample presence set 
   # original mutation indices from input_data are recorded in $mutation_indices
+  # pres <- ifelse(input_data$y > 0 & !input_data$is_cn, 1, 0) + ifelse(input_data$is_cn & input_data$tcn != 2, 1, 0)
   pres <- ifelse(input_data$y > 0, 1, 0)
+  
   pat <- apply(pres, 1, function(x) paste0(x, collapse=""))
   types <- sort(names(table(pat)), decreasing=TRUE)
-  if (length(types) == 1) {
-    type_indices <- list()
-    type_indices[[types]] <- seq_len(nrow(input_data$y))
-  } else {
-    type_indices <- lapply(types, function(x) which(pat == x))
-    names(type_indices) <- types
-  }
+  # if (length(types) == 1) {
+  #   type_indices <- list()
+  #   type_indices[[types]] <- seq_len(nrow(input_data$y))
+  # } else {
+  #   type_indices <- lapply(types, function(x) which(pat == x))
+  #   names(type_indices) <- types
+  # }
+  type_indices <- lapply(types, function(x) which(pat == x))
+  names(type_indices) <- types
   
   sep_list <- list()
   for (t in seq_len(length(types))) {
     sep_list[[types[t]]] <- list(pattern = types[t],
                                  mutation_indices = type_indices[[types[t]]],
-                                 cncf = input_data$cncf[type_indices[[types[t]]], ,drop=FALSE],
                                  y = input_data$y[type_indices[[types[t]]], ,drop=FALSE],
                                  n = input_data$n[type_indices[[types[t]]], ,drop=FALSE],
                                  tcn = input_data$tcn[type_indices[[types[t]]], ,drop=FALSE],
-                                 m = input_data$m[type_indices[[types[t]]], ,drop=FALSE])
-    if (ncol(input_data$y) == 1) {
-      break
-    }
+                                 is_cn = input_data$is_cn[type_indices[[types[t]]]],
+                                 MutID = input_data$MutID[type_indices[[types[t]]]])
+    # if (ncol(input_data$y) == 1) {
+    #   break
+    # }
   }
   return(sep_list)
 }
